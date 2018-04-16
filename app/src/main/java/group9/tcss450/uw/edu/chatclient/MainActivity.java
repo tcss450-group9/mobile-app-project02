ab3efb79d2349f64e433c29a56060fcd013b49f2
Cory <fizzy.gutsnipe@outlook.com>
2018-04-16 14:15:55 -0700

    Initial commit - 4/16/18

---
+++ app/src/main/java/group9/tcss450/uw/edu/chatclient/MainActivity.java
@@ -0,0 +1,13 @@
+package group9.tcss450.uw.edu.chatclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
